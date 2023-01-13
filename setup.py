8e8fcf690841bebac448b4b20e994c3593f2baea
RussellDash332 <russellsaerang@gmail.com>
2023-01-13 20:57:41 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup_args = dict(
+    name='mission00',
+    version='1.0',
+    description='Package setup for Mission 0',
+    packages=find_packages(),
+    author='Russell Saerang',
+    install_requires = [
+        'pillow',
+        'seaborn',
+        'scipy',
+        'https://github.com/cs1010s/cocos/releases/download/2220/cocos.zip'
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_args, include_package_data=True)
